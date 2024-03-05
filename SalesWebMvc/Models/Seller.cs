f85d04d0659629fa1624efa8870d7bce6731ffc8
Jeidsan Pereira <jeidsan.pereira@gmail.com>
2024-03-05 19:37:10 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,7 +1,4 @@
-﻿using Org.BouncyCastle.Asn1.Cmp;
-using System.Linq;
-
-namespace SalesWebMvc.Models
+﻿namespace SalesWebMvc.Models
 {
     public class Seller
     {
@@ -11,12 +8,10 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
-        public Seller()
-        {
-
-        }
+        public Seller() { }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
